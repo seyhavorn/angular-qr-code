534f094b4d0b0e83608f86bea0a492a701cdc36c
Seyha Vorn <seyha.vorn@bronxtechnology.com>
2023-03-22 17:04:45 +0700

    Updatre

--- src/app/skeletion/skeletion.component.ts
+++ src/app/skeletion/skeletion.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-skeletion',
@@ -7,12 +7,8 @@ import { Component } from '@angular/core';
 })
 export class SkeletionComponent {
 
-  isLoading: boolean = true;
+  constructor() {}
 
-  constructor() {
-    // setTimeout(() => {
-    //   this.isLoading = false;
-    // }, 3000);
+  openWebcam() {
   }
-
 }
